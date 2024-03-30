a715079a24b347af029e905da98297afc69e6442
TheCyphr <conorlowry@live.ie>
2024-03-31 00:12:21 +0900

    Solved Eight Queens Puzzle using place queen per column approach.

---
+++ EightQueensSolution/Program.cs
@@ -0,0 +1,178 @@
+﻿using System.Drawing;
+
+const int attacked = 1;
+
+var result = string.Join('\n', Solve(CreateBoard()).Select(points => string.Join(", ", points)));
+Console.WriteLine(result.Length > 0 ? $"Solutions found: \n{result}" : "No solutions found :(");
+
+return;
+
+static int[,] CreateBoard(int size = 8)
+{
+    return new int[size, size];
+}
+
+static int[,] CloneBoard(int[,] board)
+{
+    var clone = new int[board.GetLength(0),board.GetLength(1)];
+
+    foreach (var point in GetPoints(board))
+    {
+        clone[point.Y, point.X] = board[point.Y, point.X];
+    }
+
+    return clone;
+}
+
+static void PlaceQueen(int[,] board, Point point)
+{
+    foreach (var square in GetAttackingPoints(board, point))
+    {
+        board[square.Y, square.X] = attacked;
+    }
+}
+
+static IEnumerable<Point> GetPoints(int[,] board)
+{
+    for (int y = 0; y < board.GetLength(0); y++)
+    {
+        for (int x = 0; x < board.GetLength(1); x++)
+        {
+            yield return new Point(x, y);
+        }
+    }
+}
+
+static IEnumerable<Point> GetVerticalPoints(int[,] board, int x)
+{
+    if (x < 0 || x >= board.GetLength(1))
+    {
+        yield break;
+    }
+    
+    for (int y = 0; y < board.GetLength(0); y++)
+    {
+        yield return new Point(x, y);
+    }
+}
+
+static IEnumerable<Point> GetHorizontalPoints(int[,] board, int y)
+{
+    if (y < 0 || y >= board.GetLength(0))
+    {
+        yield break;
+    }
+    
+    for (int x = 0; x < board.GetLength(1); x++)
+    {
+        yield return new Point(x, y);
+    }
+}
+
+static IEnumerable<Point> GetLeadingDiagonalPoints(int[,] board, Point point)
+{
+    var lowest = int.Min(point.X, point.Y);
+    var startPoint = new Point(point.X - lowest, point.Y - lowest);
+
+    for (; startPoint.X < board.GetLength(1) && startPoint.Y < board.GetLength(0); startPoint.X++, startPoint.Y++)
+    {
+        yield return startPoint;
+    }
+}
+
+static IEnumerable<Point> GetAntiDiagonalPoints(int[,] board, Point point)
+{
+    var xDistanceFromEdge = board.GetLength(1) - 1 - point.X;
+    var lowest = int.Min(xDistanceFromEdge, point.Y);
+    var startPoint = new Point(point.X + lowest, point.Y - lowest);
+
+    for (; startPoint.X >= 0 && startPoint.Y < board.GetLength(0); startPoint.X--, startPoint.Y++)
+    {
+        yield return startPoint;
+    }
+}
+
+static IEnumerable<Point> GetAttackingPoints(int[,] board, Point point)
+{
+    foreach (var item in GetVerticalPoints(board, point.X)
+        .Concat(GetHorizontalPoints(board, point.Y))
+        .Concat(GetLeadingDiagonalPoints(board, point))
+        .Concat(GetAntiDiagonalPoints(board, point)))
+    {
+        yield return item;
+    }
+}
+
+static List<Point[]> Solve(int[,] board)
+{
+    return SolveRecursively(board, 0, []);
+
+    List<Point[]> SolveRecursively(int[,] board, int col, IEnumerable<Point> placedQueens)
+    {
+        var results = new List<Point[]>();
+
+        foreach (var point in GetLowestAttackCountPoints(board, col))
+        {
+            var newBoard = CloneBoard(board);
+            PlaceQueen(newBoard, point);
+            
+            var result = SolveRecursively(newBoard, col + 1, placedQueens.Append(point));
+            results.AddRange(result);
+        }
+
+        if (col == board.GetLength(0))
+        {
+            results.Add(placedQueens.ToArray());
+        }
+
+        return results;
+    }
+}
+
+static IEnumerable<Point> GetLowestAttackCountPoints(int[,] board, int col)
+{
+    return GetVerticalPoints(board, col)
+        .Aggregate((Result: new List<Point>(), LowestAttackCount: int.MaxValue), (result, point) =>
+        {
+            var (lowestAttackCountPoints, lowestAttackCount) = result;
+            var attackCount = GetAttackCount(board, point);
+
+            if (attackCount == -1)
+            {
+                return result;
+            }
+
+            if (attackCount == lowestAttackCount)
+            {
+                lowestAttackCountPoints.Add(point);
+            }
+            else if (attackCount < lowestAttackCount)
+            {
+                lowestAttackCount = attackCount;
+                lowestAttackCountPoints.Clear();
+                lowestAttackCountPoints.Add(point);
+            }
+
+            return (lowestAttackCountPoints, lowestAttackCount);
+        }).Result;
+}
+
+static int GetAttackCount(int[,] board, Point point)
+{
+    if (PointIsOffBoard() || CurrentSquareIsEliminated())
+    {
+        return -1;
+    }
+    
+    const int duplicateSquareCount = 3;
+
+    return GetAttackingPoints(board, point)
+        .Select(point => board[point.Y, point.X])
+        .Count(square => square is not attacked) - duplicateSquareCount;
+
+    bool PointIsOffBoard() => point.Y < 0 ||
+                              point.Y >= board.GetLength(0) && 
+                              point.X < 0 || 
+                              point.X >= board.GetLength(1);
+    bool CurrentSquareIsEliminated() => board[point.Y, point.X] is attacked;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# eight-queens-puzzle
+# Eight Queens Puzzle
+This repo provides a solution to the eight queens puzzle, which asks you to place 8 queens on a chessboard such that no queen is attacking any other queen. 
+The solution provided places a queen on a column by column basis and picks the row that attacks the lowest possible number of remaining non-attacked squares.
